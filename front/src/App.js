b29624b8a1c7c7437664aa6b033e6210c205339a
cspark0610 <cpark0610@gmail.com>
2021-03-09 19:10:28 -0300

    login y register

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+        <Route path='/login'><Login /></Route>
+        <Route path='/register'><Register /></Route>
+        
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
