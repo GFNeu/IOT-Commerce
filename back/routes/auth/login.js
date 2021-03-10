37062b5d2a46d517c21e6032585697f612e3565b
SantuchoSJ <santiagojoaquinsantucho@gmail.com>
2021-03-10 14:25:10 -0300

    detalles corregidos

--- back/routes/auth/login.js
+++ back/routes/auth/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../../models");
+const  User  = require("../../models/");
 const jwt = require("jsonwebtoken");
 
 router.post("/", (req, res, next) => {
