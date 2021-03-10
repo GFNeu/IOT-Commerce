0c4a435d4d90f953970616a4e920088f65bc5c02
patriciodfernandez <patricio.dfernandez@gmail.com>
2021-03-10 12:49:07 -0300

    jwt register/login

---
+++ back/routes/auth/login.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  User.findOne({
+    where: {
+      email,
+    },
+  })
+    .then((usuario) => {
+      if (!usuario) res.status(400).send("ingrese un email válido");
+      if (!usuario.validatePassword(password))
+        res.status(401).send("contraseña incorrecta");
+      jwt.sign(
+        {
+          id: usuario.id,
+          email: usuario.email,
+             },
+        "IOTKEY",
+        (err, token) => {
+          res.json(token);
+        }
+      );
+    })
+    .catch(next);
+});
+
+module.exports = router;
