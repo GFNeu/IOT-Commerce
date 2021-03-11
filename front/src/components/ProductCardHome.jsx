09690b8a90ce1f042dbf9d069a9745e079f8d2dd
Guille <guillermof.neuberger@gmail.com>
2021-03-11 10:41:25 -0300

    avances en el front

---
+++ front/src/components/ProductCardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCardHome = ({name, id, price, img, rating}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductCardHome
