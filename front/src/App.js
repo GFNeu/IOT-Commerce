b602cd64a88c648e8a5fa6d6c2c030edcffde5b4
patriciodfernandez <patricio.dfernandez@gmail.com>
2021-03-15 11:49:42 -0300

    conflictos admin

--- front/src/App.js
+++ front/src/App.js
@@ -11,7 +11,7 @@ import Products from "./components/Products";
 import ProductDetail from "./components/ProductDetail";
 import Footer from "./components/Footer.jsx";
 import Cart from "./components/Cart.jsx";
- 
+
 import Reviews from "./components/Reviews.jsx";
 import OrderDetail from "./components/OrderDetail.jsx";
 
@@ -22,7 +22,7 @@ import UsuariosAdmin from "./components/admin-views/UsuariosAdmin.jsx";
 import OrdenesAdmin from "./components/admin-views/OrdenesAdmin.jsx";
 import OrdenesUsuario from "./components/admin-views/OrdenesUsuario.jsx";
 import "./App.css";
- 
+
 function App() {
   const dispatch = useDispatch();
 
@@ -43,58 +43,34 @@ function App() {
       <AppBar />
       <div className="main">
         <Switch>
-          <Route path exact="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route
-            path="/products/:id"
-            render={({ match }) => <ProductDetail id={match.params.id} />}
+          <Route path exact="/"><Home /></Route>
+          <Route path="/login"><Login /></Route>
+          <Route path="/register"><Register /></Route>
+          <Route path="/products/:id/reviews" render={({ match }) => <Reviews id={match.params.id} />}
           />
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/courses/:id">
-            <NoDisponible />
-          </Route>
+
+          <Route path="/products/:id" render={({ match }) => <ProductDetail id={match.params.id} />}
+          />
+          <Route path="/products"><Products /></Route>
+          <Route path="/cart"><Cart /></Route>
+          <Route path="/courses/:id"><NoDisponible /></Route>
           {/* <Route path ="/products/detail" component={ProductDetail}/> */}
 
-          <Route exact path="/adminPanel">
-            <AdminPanel />
+          <Route exact path="/adminPanel"><AdminPanel /></Route>
+
+          <Route exact path="/adminPanel/productos"><ProductosAdmin />   </Route>
+
+          <Route exact path="/adminPanel/usuarios"><UsuariosAdmin />
           </Route>
 
-          <Route exact path="/adminPanel/productos">
-            <ProductosAdmin />
-          </Route>
-
-          <Route exact path="/adminPanel/usuarios">
-            <UsuariosAdmin />
-          </Route>
-
-          <Route exact path="/adminPanel/ordenes">
-            <OrdenesAdmin />
+          <Route exact path="/adminPanel/ordenes"><OrdenesAdmin />
           </Route>
 
           <Route exact path="/adminPanel/usuarios/ordenesUsuario">
             <OrdenesUsuario />
           </Route>
 
-          <Route
-            path="/products/:id/reviews"
-            render={({ match }) => <Reviews id={match.params.id} />}
-          />
-
-          <Route path="/order">
-            <OrderDetail />
-          </Route>
+          <Route path="/order"><OrderDetail /></Route>
         </Switch>
       </div>
       <Footer />
