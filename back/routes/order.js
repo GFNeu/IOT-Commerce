f39fda7ca65e2103fc1115f6636706ed929dd7d9
Guille <guillermof.neuberger@gmail.com>
2021-03-16 17:15:12 -0300

    rutas del carrito para user funcionando en el back. Cambios en los modelos orders y ProductOrders

--- back/routes/order.js
+++ back/routes/order.js
@@ -1,33 +1,23 @@
 const router = require('express').Router()
-const {createOrder} = require('../controllers/order')
+const {createOrder, getPendingOrder, updateOrder, checkout, removeOrder, getPastOrders} = require('../controllers/order')
 
 //get la orden que este pending
-router.get('/pending', (req,res,next)=>{
-    res.send("esta es la ruta de órdenes GET PENDING")
-})
+router.get('/pending', getPendingOrder)
 
 //get todas las órdenes completas
-router.get('/', (req,res,next)=>{
-    res.send("esta es la ruta de órdenes GET ALL")
-})
-
-//MODIFICAR UNA ORDEN
-router.put('/', (req,res,next)=>{
-    res.send("esta es la ruta de órdenes PUT")
-})
+router.get('/', getPastOrders)
 
 //CHECKOUT
-router.post('/checkout', (req,res,next)=>{
-    res.send("esta es la ruta de órdenes CHECKOUT")
-})
+router.put('/checkout', checkout)
+
+//MODIFICAR UNA ORDEN
+router.put('/', updateOrder)
 
 //CREATE ORDER
 router.post('/', createOrder)
 
 //BORRAR ORDEN
-router.delete('/', (req,res,next)=>{
-    res.send("esta es la ruta de órdenes DELETE")
-})
+router.delete('/', removeOrder)
 
 
 
