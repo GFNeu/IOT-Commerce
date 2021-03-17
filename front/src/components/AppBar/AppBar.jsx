be2f7d342051cd94432e50f76b03082a6a011807
cspark0610 <cpark0610@gmail.com>
2021-03-17 15:44:11 -0300

    aaaaaa

--- front/src/components/AppBar/AppBar.jsx
+++ front/src/components/AppBar/AppBar.jsx
@@ -46,7 +46,6 @@ const AppBar = ( ) => {
       dispatch(getProductsByKeyword(keyword))
       //.then(()=>window.location = `/products/search?name=${keyword}`)      
        .then(()=>history.push ( `/products/search?name=${keyword}`))      
-     
     }
     else history.push('/')
   };
