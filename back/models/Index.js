57088c7fda4310be75a970b89ecdc5f5fc17bf0c
jerickert <jerickert@gmail.com>
2021-03-17 10:23:34 -0300

    miercoles de manana antes del push de Guille

--- back/models/Index.js
+++ back/models/Index.js
@@ -8,14 +8,6 @@ const OrdersProducts= require("./OrderProducts")
 const db = require("../db/index");
 const ProductCategories = require("./ProductCategories");
 
-
-// ACA, EN EL INDEX, IMPORTAMOS TODOS LOS MODELOS Y HACEMOS LAS RELACIONES
-// EXPORTAMOS TODOS LOS MODELOS, ESTE INDEX, ES EL UNICO PUNTO DE CONTACTO CON EL EXTERIOR
-// EN LO QUE HACE A MODELOS DE DATOS Y SUS RELACIONES
-
-// RECORDAR QUE LAS RELACIONES AGREGAN METODOS
-
-
 // REVIEWS
 User.hasMany(Reviews);
 Reviews.belongsTo(Products);
@@ -28,10 +20,6 @@ OrderStatus.hasMany(Order);
 Order.belongsToMany(Products, {through: OrdersProducts});
 Products.belongsToMany(Order, {through: OrdersProducts});
 
-// PRODUCTS
-// Categories.belongsToMany(Products, {through: "products_categories"});
-// Products.belongsToMany(Categories,{through: "products_categories"});
-// Products belongToMany Tags (through ProductTag)
 Products.belongsToMany(Categories, {
     through: ProductCategories,
     foreignKey: 'product_id',
